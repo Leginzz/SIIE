416abc0b9dd0eaf971c1b8f91b23ecfc8701bafd
Leginzz <luisgaciazz@gmail.com>
2026-06-16 21:09:32 -0600

    Configurar Firebase para producción

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDJxYwe-ZDNhiGaIPZz2U_AVRk3GE3Lw",
+  authDomain: "siie-fb7ab.firebaseapp.com",
+  projectId: "siie-fb7ab",
+  storageBucket: "siie-fb7ab.firebasestorage.app",
+  messagingSenderId: "320632854500",
+  appId: "1:320632854500:web:77e195460c3c7255c299f1"
 };
 
 export const hasFirebaseConfig = Object.values(firebaseConfig).every(Boolean);
